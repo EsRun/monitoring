4321758e6b7b34d53d964bd40b9b1c808fcc8b8f
비엔나 <sure@paran.com>
2023-09-11 13:58:35 +0900

    test : zoom init issue

--- src/components/maps/MapDraw.tsx
+++ src/components/maps/MapDraw.tsx
@@ -22,7 +22,7 @@ const MapDraw = () => {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
-
+    map.setZoom(10);
     setMap(map);
   }, []);
 
@@ -35,7 +35,7 @@ const MapDraw = () => {
       mapContainerStyle={containerStyle}
       center={center}
       zoom={8}
-      onLoad={onLoad}
+      //onLoad={onLoad}
       onUnmount={onUnmount}
     >
       {/* Child components, such as markers, info windows, etc. */}
